feb4aa9272389e91b8610094ddc7cf9494c2fac9
Donald Patterson <donald.patterson@gmail.com>
2021-12-01 10:03:07 -0400

    first day

---
+++ 2021/src/day-01.py
@@ -0,0 +1,29 @@
+from aoc_common import load_ints
+
+input = load_ints("input-01-1")
+
+# part 1
+[prev, *remaining] = input
+increase_count = 0
+
+for value in remaining:
+    if prev < value:
+        increase_count += 1
+    prev = value
+
+print("1:", increase_count)
+
+
+# part 2
+group_count = len(input) - 2
+groups = [input[i : i + 3] for i in range(group_count)]
+
+[prev, *remaining] = [sum(group) for group in groups]
+increase_count = 0
+
+for value in remaining:
+    if prev < value:
+        increase_count += 1
+    prev = value
+
+print("2:", increase_count)
---
+++ README.md
@@ -0,0 +1,5 @@
+Halihax is doing [Advent of Code](https://adventofcode.com/2021)! We have a [leaderboard](https://adventofcode.com/2021/leaderboard/private/view/288354) as well.
+
+Reminder for next year(s):
+
+- You signed in using your GitHub account `deek80`. Don't lose your previous data again!
