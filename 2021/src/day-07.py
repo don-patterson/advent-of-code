59f912adb74262b66f8b663311c4e0f799f5936f
Donald Patterson <donald.patterson@gmail.com>
2021-12-07 10:14:24 -0400

    trim

--- 2021/src/day-07.py
+++ 2021/src/day-07.py
@@ -4,25 +4,13 @@ positions = []
 for line in lines("input-07.txt"):
     positions.extend(int(i) for i in line.split(","))
 
-
-def cost1(a, b):
-    return abs(a - b)
+start, end = min(positions), max(positions)
 
 
-def cost2(a, b):
-    diff = cost1(a, b)
-    return int((diff * (diff + 1)) / 2)
+def cost(a, b):
+    d = abs(a - b)
+    return d * (d + 1) // 2
 
 
-min_cost1 = None
-min_cost2 = None
-for p in range(min(positions), max(positions)):
-    c1 = sum(cost1(p, i) for i in positions)
-    c2 = sum(cost2(p, i) for i in positions)
-    if min_cost1 is None or c1 < min_cost1:
-        min_cost1 = c1
-    if min_cost2 is None or c2 < min_cost2:
-        min_cost2 = c2
-
-print("1:", min_cost1)
-print("2:", min_cost2)
+print("1:", min(sum(abs(p - i) for i in positions) for p in range(start, end)))
+print("2:", min(sum(cost(p, i) for i in positions) for p in range(start, end)))
