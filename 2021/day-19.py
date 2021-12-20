b4ab46f940f402eb2c13b953c22849f610d63de3
Donald Patterson <donald.patterson@gmail.com>
2021-12-20 10:07:17 -0400

    this was hella slow, but technically correct

--- 2021/day-19.py
+++ 2021/day-19.py
@@ -1,81 +1,78 @@
-from itertools import combinations, pairwise, takewhile
+from itertools import combinations, pairwise, permutations, takewhile
+from functools import reduce
+from collections import Counter
 
 
-projections = {
-    "+x": lambda p: p[0],
-    "-x": lambda p: -p[0],
-    "+y": lambda p: p[1],
-    "-y": lambda p: -p[1],
-    "+z": lambda p: p[2],
-    "-z": lambda p: -p[2],
-}
+def all_rotations():
+    """find all of the 24 rotations, based on where (1,2,3) ends up
+    after a bunch of x, y, and z rotations
+    """
+    base_rotations = {
+        "x": lambda p: (p[0], -p[2], p[1]),
+        "y": lambda p: (p[2], p[1], -p[0]),
+        "z": lambda p: (-p[1], p[0], p[2]),
+    }
+
+    start = (1, 2, 3)
+    origin = {
+        1: lambda p: p[0],
+        -1: lambda p: -p[0],
+        2: lambda p: p[1],
+        -2: lambda p: -p[1],
+        3: lambda p: p[2],
+        -3: lambda p: -p[2],
+    }
+
+    def get_transform(end):
+        """find a simplified function that moves `(1,2,3)` to `end`"""
+        return lambda p: (origin[end[0]](p), origin[end[1]](p), origin[end[2]](p))
+
+    observed = set()
+    for x in range(4):
+        for y in range(4):
+            for z in range(4):
+                rotations = (
+                    [base_rotations["x"]] * x
+                    + [base_rotations["y"]] * y
+                    + [base_rotations["z"]] * z
+                )
+                result = reduce(lambda point, rot: rot(point), rotations, start)
+                if result not in observed:
+                    yield get_transform(result)
+                    observed.add(result)
 
 
-def offsets(values):
-    m = min(values)
-    return set(v - m for v in values)
+rotations = list(all_rotations())
+
+
+def add(v1, v2):
+    return tuple(a + b for a, b in zip(v1, v2))
 
 
 class Scanner:
-    def __init__(self, id):
+    def __init__(self, id, points=None):
         self.id = id
-        self.points = []
+        self.points = points or []
 
-    @property
-    def first(self):
-        return self.points[0]
+    def coords(self, d):
+        i = ["x", "y", "z"].index(d)
+        return [p[i] for p in self.points]
 
-    @property
-    def last(self):
-        return self.points[-1]
+    def count(self, d):
+        return Counter(self.coords(d))
 
-    def project(self, d):
-        proj = projections[d]
-        for p in self.points:
-            yield proj(p)
+    def rotate(self, rotation):  # x, y, z
+        return Scanner(self.id, [rotation(p) for p in self.points])
 
-    def offsets(self, d):
-        return offsets(list(self.project(d)))
+    def translate(self, q):
+        return Scanner(self.id, [add(p, q) for p in self.points])
+
+    def info(self):
+        print(f"Scanner {self.id} has {len(self.points)} points", self.count("x"))
 
     def __repr__(self) -> str:
         return f"S({self.id})"
         # return f"S[{self.id}:{self.first}...{self.last}]"
-
-
-def max_overlap(offsets1, offsets2):
-    """slide offsets2 to the right until it maximally overlaps with offsets1
-    i.e. offsets1 = {0, 2, 7, 12, 15}
-     and offsets2 = {0, 5, 8}
-
-    try:
-     0:          offsets1 = {0, 2, 7, 12, 15}
-       intersect offsets2 = {0, 5, 8}
-
-     1:          offsets1 =     {0, 2, 7, 12, 15}
-       intersect offsets2 + 1 = {1, 6, 9}
-     ...
-     7:          offsets1 =     {0, 2, 7, 12, 15}
-       intersect offsets2 + 7 = {7, 12, 15}
-              for maximal intersection of 3
-     ...
-     15: would be the last attempt
-    """
-    best = (0, 0)  # (size, slide)
-    for slide in range(max(offsets1) + 1):
-        adjusted_offsets2 = {e + slide for e in offsets2}
-        size = len(offsets1 & adjusted_offsets2)
-        if size > best[0]:
-            best = (size, slide)
-    return best
-
-
-def find_overlap(s0, s1):
-    for d0 in ["+x", "+y", "+z"]:
-        off0 = s0.offsets(d0)
-        for d1 in projections:
-            size, slide = max_overlap(off0, s1.offsets(d1))
-            if size >= 12:
-                print(s0.id, s1.id, "overlaps", d0, d1, size, slide)
 
 
 scanners = []
@@ -87,5 +84,112 @@ with open("input-19.txt") as f:
         scanners.append(s)
 
 
-for s0, s1 in combinations(scanners, 2):
-    find_overlap(s0, s1)
+def intersects(count1, count2):
+    """slide count2 from left to right until it aligns best with count1"""
+    min1, max1 = min(count1.keys()), max(count1.keys())
+    min2, max2 = min(count2.keys()), max(count2.keys())
+    for offset in range(min1 - max2, max1 - min2 + 1):
+        off_count2 = {coord + offset: count for coord, count in count2.items()}
+        intersection = Counter(
+            {
+                k: min(count1[k], off_count2[k])
+                for k in count1.keys() & off_count2.keys()
+            }
+        )
+        if intersection.total() >= 12:
+            return offset
+    return None
+
+
+def find_aligned(s0, s1):
+    x = s0.count("x")
+    y = s0.count("y")
+    z = s0.count("z")
+    for r in rotations:
+        s = s1.rotate(r)
+        ox = intersects(x, s.count("x"))
+        if ox is None:
+            continue
+        oy = intersects(y, s.count("y"))
+        if oy is None:
+            continue
+        oz = intersects(z, s.count("z"))
+        if oz is None:
+            continue
+        # confirm
+        s = s.translate((ox, oy, oz))
+        if len(set(s0.points) & set(s.points)) < 12:
+            continue
+        return s
+    return None
+
+
+# for s0, s1 in combinations(scanners, 2):
+#     if find_aligned(s0, s1) is not None:
+#         print(s0.id, s1.id, "intersect")
+# 0 2 intersect
+# 0 24 intersect
+# 0 27 intersect
+# 1 15 intersect
+# 1 27 intersect
+# 1 32 intersect
+# 1 36 intersect
+# 2 18 intersect
+# 3 6 intersect
+# 3 19 intersect
+# 3 20 intersect
+# 3 32 intersect
+# 4 11 intersect
+# 4 12 intersect
+# 4 27 intersect
+# 5 13 intersect
+# 6 9 intersect
+# 6 34 intersect
+# 6 35 intersect
+# 7 18 intersect
+# 8 12 intersect
+# 9 13 intersect
+# 9 17 intersect
+# 9 20 intersect
+# 10 15 intersect
+# 10 23 intersect
+# 12 30 intersect
+# 13 21 intersect
+# 14 25 intersect
+# 14 28 intersect
+# 15 26 intersect
+# 16 29 intersect
+# 16 33 intersect
+# 16 36 intersect
+# 17 34 intersect
+# 18 23 intersect
+# 19 35 intersect
+# 22 33 intersect
+# 23 26 intersect
+# 24 31 intersect
+# 25 29 intersect
+# 26 27 intersect
+# 28 29 intersect
+# 28 36 intersect
+
+
+aligned = [scanners[0]]
+remaining = scanners[1:]
+
+
+def whatsleft():
+    for a in reversed(aligned):
+        for r in remaining:
+            yield a, r
+
+
+while remaining:
+    print(len(remaining), "remain")
+    for a, r in whatsleft():
+        new_r = find_aligned(a, r)
+        if new_r:
+            remaining.remove(r)
+            aligned.append(new_r)
+
+print("aligned!", aligned)
+print("1:", len(set.union(*[set(a.points) for a in aligned])))
