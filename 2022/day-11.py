39574491cb1fea60404883758560eac454889dd4
Donald Patterson <donald.patterson@gmail.com>
2022-12-11 14:39:55 -0400

    day 11 part2, done

--- 2022/day-11.py
+++ 2022/day-11.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from math import prod
 from re import findall
 
 monkies = []
@@ -16,11 +18,24 @@ with open("input/11.txt") as f:
         }
         monkies.append(monkey)
 
+monkies_copy = deepcopy(monkies)
+
+max_div = prod(m["test"] for m in monkies)
+
 
 def turn1(monkey):
     for item in monkey["items"]:
         monkey["count"] += 1
         worry = monkey["op"](item) // 3
+        target = monkey["target1"] if worry % monkey["test"] == 0 else monkey["target2"]
+        monkies[target]["items"].append(worry)
+    monkey["items"].clear()
+
+
+def turn2(monkey):
+    for item in monkey["items"]:
+        monkey["count"] += 1
+        worry = monkey["op"](item) % max_div
         target = monkey["target1"] if worry % monkey["test"] == 0 else monkey["target2"]
         monkies[target]["items"].append(worry)
     monkey["items"].clear()
@@ -32,3 +47,12 @@ for i in range(20):
 
 m1, m2 = sorted(m["count"] for m in monkies)[-2:]
 print("1:", m1 * m2)
+
+
+monkies = monkies_copy
+for i in range(10_000):
+    for m in monkies:
+        turn2(m)
+
+m1, m2 = sorted(m["count"] for m in monkies)[-2:]
+print("2:", m1 * m2)
